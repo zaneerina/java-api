6aeac71f11cb980b9ad1cd151cf4904d92dbb079
zaneerina <zane.erina@gmail.com>
2023-02-10 08:44:37 +0200

    maven setup with testng - a simple test ran from testng.xml file

---
+++ api-testing/src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+import org.testng.annotations.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("testing");
+    }
+
+}
---
+++ api-testing/src/main/java/TestNG.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class TestNG {
+    @Test
+    public void testTheTestNgSetup (){
+        System.out.println("testng test..");
+    }
+}
