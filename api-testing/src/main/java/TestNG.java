bf4bdd2ec47a8d8c12e6be1988af2288d210fda2
zaneerina <zane.erina@gmail.com>
2023-02-12 11:34:16 +0200

    added an excluded method to the testng.xml file

--- api-testing/src/main/java/TestNG.java
+++ api-testing/src/main/java/TestNG.java
@@ -5,4 +5,19 @@ public class TestNG {
     public void testTheTestNgSetup (){
         System.out.println("testng test..");
     }
+
+    @Test
+    public void method1 (){
+        System.out.println("testng test1..");
+    }
+    @Test
+    public void method2 (){
+        System.out.println("testng test2..");
+    }
+
+    @Test
+    public void method3 (){
+        System.out.println("testng test3..");
+    }
+
 }
